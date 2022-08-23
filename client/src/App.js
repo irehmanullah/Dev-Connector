38df09c638e05abb8551a8194fc5266382f48cf2
Rehman Ullah <irehmanullah@gmail.com>
2022-08-23 11:06:15 +0500

    Register form front-end created

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Outlet, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Navbar from './Components/layout/Navbar';
 import Landing from './Components/layout/Landing'
