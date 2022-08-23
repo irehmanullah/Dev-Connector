d3accf657e17b09b34e580feba6d5a4af11dc65e
Rehman Ullah <irehmanullah@gmail.com>
2022-08-23 10:50:49 +0500

    Basic Route setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Outlet, Navigate } from 'react-router-dom';
 
 import Navbar from './Components/layout/Navbar';
 import Landing from './Components/layout/Landing'
+import Login from './Components/auth/Login';
+import Register from './Components/auth/Register';
 import './App.css';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Landing />
-    </>
+      <Routes>
+        <Route exact path='/' element={<Landing />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/register' element={<Register />} />
+      </Routes>
+    </Router>
   );
 };
 
