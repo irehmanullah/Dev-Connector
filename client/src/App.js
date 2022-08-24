049a2da875d81a73a9778b8c8ac23b592177c292
Rehman Ullah <irehmanullah@gmail.com>
2022-08-24 13:33:53 +0500

    user auth function created

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import Navbar from "./Components/layout/Navbar";
 import Landing from "./Components/layout/Landing";
 import Login from "./Components/auth/Login";
 import Register from "./Components/auth/Register";
+import { loadUser } from './actions/auth';
 import "./App.css";
 import store from "./store";
 import Alert from './Components/layout/Alert';
+import setAuthToken from './utils/setAuthToken';
+
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token)
+}
 
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
   return (
     <Provider store={store}>
       <Router>
--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
+import setAuthToken from '../utils/setAuthToken';
 import { setAlert } from "./alert";
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from "./types";
 
 /*
   NOTE: we don't need a config object for axios as the
@@ -8,6 +9,25 @@ import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
  also axios stringifies and parses JSON for you, so no need for 
  JSON.stringify or JSON.parse
 */
+
+// Load User
+export const loadUser = () => async (dispatch) => {
+    if (localStorage.token) {
+        setAuthToken(localStorage.token)
+    }
+    try {
+        const res = await axios.get('/api/auth');
+
+        dispatch({
+            type: USER_LOADED,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: AUTH_ERROR
+        });
+    }
+};
 
 // Register User
 export const register = (formData) => async (dispatch) => {
