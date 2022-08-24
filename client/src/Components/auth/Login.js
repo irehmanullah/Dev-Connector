dccb33319dcc0b50e4a51f66a8d1e6114edfc898
Rehman Ullah <irehmanullah@gmail.com>
2022-08-24 19:07:49 +0500

    login work is done

--- client/src/Components/auth/Login.js
+++ client/src/Components/auth/Login.js
@@ -1,57 +1,73 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { login } from "../../actions/auth";
 
-const Login = () => {
-    const [formData, setFormData] = useState({
-        email: "",
-        password: "",
-    });
+const Login = ({ login, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const { email, password } = formData;
+  const { email, password } = formData;
 
-    const onChange = (e) =>
-        setFormData({ ...formData, [e.target.name]: e.target.value });
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        console.log(formData);
-    };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    login(email, password);
+  };
 
-    return (
-        <section className="container">
-            <h1 className="large text-primary">Sign In</h1>
-            <p className="lead">
-                <i className="fas fa-user"></i> Sing Into Your Account
-            </p>
-            <form className="form" onSubmit={onSubmit}>
-                <div className="form-group">
-                    <input
-                        type="email"
-                        placeholder="Email Address"
-                        name="email"
-                        value={email}
-                        onChange={(e) => onChange(e)}
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        name="password"
-                        minLength="6"
-                        value={password}
-                        onChange={(e) => onChange(e)}
-                        required
-                    />
-                </div>
-                <input type="submit" className="btn btn-primary" value="Login" />
-            </form>
-            <p className="my-1">
-                Don't have an account? <Link to="/register">Sign Up</Link>
-            </p>
-        </section>
-    );
+  if (isAuthenticated) {
+    return <Navigate to="/dashboard" />;
+  }
+
+  return (
+    <section className="container">
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sing Into Your Account
+      </p>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </section>
+  );
 };
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { login })(Login);
--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,7 +1,14 @@
-import axios from 'axios'
-import setAuthToken from '../utils/setAuthToken';
+import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
 import { setAlert } from "./alert";
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from "./types";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+} from "./types";
 
 /*
   NOTE: we don't need a config object for axios as the
@@ -12,40 +19,67 @@ import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from "./type
 
 // Load User
 export const loadUser = () => async (dispatch) => {
-    if (localStorage.token) {
-        setAuthToken(localStorage.token)
-    }
-    try {
-        const res = await axios.get('/api/auth');
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+  try {
+    const res = await axios.get("/api/auth");
 
-        dispatch({
-            type: USER_LOADED,
-            payload: res.data
-        });
-    } catch (err) {
-        dispatch({
-            type: AUTH_ERROR
-        });
-    }
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR,
+    });
+  }
 };
 
 // Register User
 export const register = (formData) => async (dispatch) => {
-    console.log(formData)
-    try {
-        const res = await axios.post("/api/users", formData);
+  console.log(formData);
+  try {
+    const res = await axios.post("/api/users", formData);
 
-        dispatch({
-            type: REGISTER_SUCCESS,
-            payload: res.data,
-        });
-    } catch (err) {
-        const errors = err.response.data.errors;
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data,
+    });
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
 
-        if (errors) {
-            errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
-        }
-
-        dispatch({ type: REGISTER_FAIL });
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
     }
+
+    dispatch({ type: REGISTER_FAIL });
+  }
+};
+
+// Login User
+export const login = (email, password) => async (dispatch) => {
+  const body = { email, password };
+
+  try {
+    const res = await axios.post("/api/auth", body);
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data,
+    });
+
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+    }
+
+    dispatch({
+      type: LOGIN_FAIL,
+    });
+  }
 };
