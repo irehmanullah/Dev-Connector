a3fa713f002e4b3b802db835d8c55072586de8e2
Rehman Ullah <irehmanullah@gmail.com>
2022-08-25 19:07:13 +0500

    Private Route Added

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,16 @@ import Navbar from "./Components/layout/Navbar";
 import Landing from "./Components/layout/Landing";
 import Login from "./Components/auth/Login";
 import Register from "./Components/auth/Register";
-import { loadUser } from './actions/auth';
+import { loadUser } from "./actions/auth";
 import "./App.css";
 import store from "./store";
-import Alert from './Components/layout/Alert';
-import setAuthToken from './utils/setAuthToken';
-
+import Alert from "./Components/layout/Alert";
+import setAuthToken from "./utils/setAuthToken";
+import Dashboard from "./Components/dashboard/Dashboard";
+import PrivateRoute from "./Components/routing/PrivateRoute";
 
 if (localStorage.token) {
-  setAuthToken(localStorage.token)
+  setAuthToken(localStorage.token);
 }
 
 const App = () => {
@@ -29,6 +30,10 @@ const App = () => {
           <Route exact path="/" element={<Landing />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/register" element={<Register />} />
+          <Route
+            path="dashboard"
+            element={<PrivateRoute component={Dashboard} />}
+          />
         </Routes>
       </Router>
     </Provider>
