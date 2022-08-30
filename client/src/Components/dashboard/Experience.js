d2b3e11fc7362e1a1c617f1e56dbee8f25c99c3c
Rehman Ullah <irehmanullah@gmail.com>
2022-08-30 11:28:00 +0500

    Education list added

--- client/src/Components/dashboard/Experience.js
+++ client/src/Components/dashboard/Experience.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Moment from "react-moment";
+
 import { deleteExperience } from "../../actions/profile";
 
 const Experience = ({ experience, deleteExperience }) => {
