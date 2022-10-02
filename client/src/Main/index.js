1ca99f14c4a900094d0a5f8d05ce380ae51a3180
irehmanullah <irehmanullah@gmail.com>
2022-10-03 00:16:42 +0500

    Routes issue solved

---
+++ client/src/Main/index.js
@@ -0,0 +1,80 @@
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Provider, useSelector } from "react-redux";
+
+import Navbar from "../Components/layout/Navbar";
+import Landing from "../Components/layout/Landing";
+import Login from "../Components/auth/Login";
+import Register from "../Components/auth/Register";
+import { loadUser } from "../actions/auth";
+import "../App.css";
+import store from "../store";
+import Alert from "../Components/layout/Alert";
+import setAuthToken from "../utils/setAuthToken";
+import Dashboard from "../Components/dashboard/Dashboard";
+import PrivateRoute from "../Components/routing/PrivateRoute";
+import ProfileForm from "../Components/profile-forms/ProfileForm";
+import AddExperience from "../Components/profile-forms/AddExperience";
+import AddEducation from "../Components/profile-forms/AddEducation";
+import Profiles from "../Components/profiles/Profiles";
+import Profile from "../Components/profile/Profile";
+import Posts from "../Components/posts/Posts";
+import Post from "../Components/post/Post";
+import NotFound from "../Components/layout/NotFound";
+import Spinner from '../Components/layout/Spinner'
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  const auth = useSelector(state => state.auth)
+  useEffect(() => {
+    if (localStorage.token) {
+      store.dispatch(loadUser());
+    }
+  }, []);
+
+  if (auth.loading) return <Spinner />
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Alert />
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route path="profiles" element={<Profiles />} />
+          <Route path="profile/:id" element={<Profile />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route
+            path="dashboard"
+            element={<PrivateRoute component={Dashboard} />}
+          />
+          <Route
+            path="create-profile"
+            element={<PrivateRoute component={ProfileForm} />}
+          />
+          <Route
+            path="edit-profile"
+            element={<PrivateRoute component={ProfileForm} />}
+          />
+          <Route
+            path="add-experience"
+            element={<PrivateRoute component={AddExperience} />}
+          />
+          <Route
+            path="add-education"
+            element={<PrivateRoute component={AddEducation} />}
+          />
+          <Route path="posts" element={<PrivateRoute component={Posts} />} />
+          <Route path="posts/:id" element={<PrivateRoute component={Post} />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
