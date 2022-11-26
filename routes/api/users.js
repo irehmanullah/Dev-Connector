11c5cd775dd369eb37367753bd7a037da1032801
irehmanullah <irehmanullah@gmail.com>
2022-11-26 22:50:29 +0500

    Added env file

--- routes/api/users.js
+++ routes/api/users.js
@@ -50,7 +50,7 @@ router.post('/', [
 
         jwt.sign(
             payload,
-            config.get('jwtSecret'),
+            process.env.jwtSecret,
             { expiresIn: 360000 },
             (err, token) => {
                 if (err) throw err;
