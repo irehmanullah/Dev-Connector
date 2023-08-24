c6667c140498c00595dc8f66fdfada9f1ba5646b
irehmanullah <irehmanullah@gmail.com>
2023-08-24 14:22:01 +0500

    firebase added

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -41,7 +41,9 @@ export const getProfiles = () => async (dispatch) => {
   dispatch({ type: CLEAR_PROFILE });
 
   try {
-    const res = await axios.get("/api/profile");
+    const res = await axios.get(
+      `${process.env.REACT_APP_BASE_URL}/api/profile`
+    );
 
     dispatch({
       type: GET_PROFILES,
