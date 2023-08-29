cfc893bf8cf8fa41934c1a420b5ed1699e878f53
irehmanullah <irehmanullah@gmail.com>
2023-08-30 00:09:09 +0500

    Added google analytics

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { useEffect } from "react";
 import { Provider } from "react-redux";
+import { initializeApp } from "firebase/app";
+import { getAnalytics, logEvent } from "firebase/analytics";
 
 import { loadUser } from "./actions/auth";
 import "./App.css";
 import store from "./store";
 import setAuthToken from "./utils/setAuthToken";
-import Main from './Main'
+import Main from "./Main";
+
+const firebaseConfig = require("./firebasekey.json");
+const fireBaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(fireBaseApp);
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -13,6 +19,7 @@ if (localStorage.token) {
 
 const App = () => {
   useEffect(() => {
+    logEvent(analytics, "userVisited");
     if (localStorage.token) {
       store.dispatch(loadUser());
     }
